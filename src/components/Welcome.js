cdbea4dad777fc94e204282e376404cc795ee193
kirstdanic <kirstenddemko@gmail.com>
2024-07-07 04:43:08 -0400

    Commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>OPERATION:visionary</h1>
+      <p>The future is in all of our hands. Time is inevitable - make it individual.</p>
+    </div>
+  );
+};
+
+export default Welcome;
